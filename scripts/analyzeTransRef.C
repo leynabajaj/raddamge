fdb3c086bf14e250af98aecefe2dbce0285af404
chandabindu <chandabindu@gmail.com>
2020-04-26 04:02:42 -0400

    Fixed some bug

--- scripts/analyzeTransRef.C
+++ scripts/analyzeTransRef.C
@@ -1,11 +1,11 @@
 TCanvas *c1=new TCanvas("c1","c1",1400,600);
-string onm="Ploy_TransRef_PDGid2112_Ecut0.pdf";
-string ofnm = "Ploy_TransRefl_PDGid2112_Ecut0.root";
-const int nEnergy = 3;
+string onm="Lead_TransRef_PDGid22_Ecut0.pdf";
+string ofnm = "Lead_TransRefl_PDGid22_Ecut0.root";
+const int nEnergy = 5;
 const int nThickness = 8;
 const int nMaterial = 1;
 const float nEv=1e5;
-int pdgID = 2112;
+int pdgID = 22;
 string eCut="preKE>0";
 
 void analyzeOneMaterial(string material, int pdgID, TGraph *g[2][2][nThickness+nEnergy], double thickness[nThickness], double effThick[nThickness]);
@@ -13,16 +13,17 @@ void extractOneFile(int e, double thick, string mat, int pdgID,double trans[2], 
 
 void analyzeTransRef(){
 
-  string materials[nMaterial] = {"Borated_Polythene"};
+  string materials[nMaterial] = {"G4_Pb"};
 		/*	 "G4_W",
 			 "G4_Pb",
 			 "G4_Fe",
+			 "Borated_Polythene",
 			 "G4_POLYETHYLENE",
 			 "G4_CONCRETE"
   };*/
   int colorMat[nMaterial]={1};
-  double thicknesses[nMaterial][nThickness]={{5,20,40,60,80,100,150,200}};
-  //double thicknesses[nMaterial][nThickness]={{1,2,5,10,15,20}};
+//  double thicknesses[nMaterial][nThickness]={{1,2,5,20,40,60,80,100,150,200}};
+  double thicknesses[nMaterial][nThickness]={{1,2,5,10,15,20,25,50}};
 			/*    {5,7.5,10,12.5}};
 			    {1.69,8.47,16.95,25.42},
 			    {2.44,12.20,24.39,36.59},
@@ -70,7 +71,7 @@ void analyzeOneMaterial(string material, int pdgID, TGraph *g[2][2][nThickness+n
   //[hits, EweightHits][thickenss][energy]
   double trans[2][nThickness][nEnergy], refl[2][nThickness][nEnergy];
   //double energy[7] = {500,1000,1500,2000,3000,5000,8000};
-  double energy[nEnergy] = {10,50,100};
+  double energy[nEnergy] = {100,150,200,250,500};
 
   for(int i = 0 ; i < nThickness ; i++ )//thickness
     for(int j = 0 ; j < nEnergy; j++){
@@ -87,19 +88,19 @@ void analyzeOneMaterial(string material, int pdgID, TGraph *g[2][2][nThickness+n
   for(int i=0;i<2;i++){
     for(int j=0;j<nThickness;j++){
       g[i][0][j] = new TGraph(nEnergy,energy,trans[i][j]);
-      g[i][0][j] -> SetTitle(Form("%s %s Trans for %d effThick %d; energy [MeV]; hits [#]",
+      g[i][0][j] -> SetTitle(Form("%s %s Trans for %d effThick %d; energy [keV]; hits [#]",
 				  material.c_str(),eW[i].c_str(), pdgID, int(effThick[j])));
       g[i][0][j] -> SetName(Form("TvsE_%s_%s_%d",
 				 material.c_str(), eW[i].c_str(), int(effThick[j])));
 
       g[i][1][j] = new TGraph(nEnergy,energy,refl[i][j]);
-      g[i][1][j] -> SetTitle(Form("%s %s Reflc for %d effThick %d; energy [MeV]; hits [#]",
+      g[i][1][j] -> SetTitle(Form("%s %s Reflc for %d effThick %d; energy [keV]; hits [#]",
 				  material.c_str(),eW[i].c_str(),pdgID, int(effThick[j])));
       g[i][1][j] -> SetName(Form("RvsE_%s_%s_%d",
 				 material.c_str(),eW[i].c_str(), int(effThick[j])));
     }
 
-    for(int j=4;j<nThickness+nEnergy;j++){
+    for(int j=nThickness;j<nThickness+nEnergy;j++){
       double tVal[nThickness],rVal[nThickness];
       for(int k=0;k<nThickness;k++){
 	tVal [k] = trans[i][k][j-nThickness];
@@ -107,14 +108,14 @@ void analyzeOneMaterial(string material, int pdgID, TGraph *g[2][2][nThickness+n
       }
       g[i][0][j] = new TGraph(nThickness,effThick,tVal);
       //g[i][0][j]->GetYaxis()->SetRangeUser(0.01,9);
-      g[i][0][j] -> SetTitle(Form("%s %s Trans for PID=%d E=%d; thickness [cm]; hits [#]",
+      g[i][0][j] -> SetTitle(Form("%s %s Trans for PID=%d E=%d; thickness [mm]; hits [#]",
 				  material.c_str(),eW[i].c_str(), pdgID,int(energy[j-nThickness])));
       g[i][0][j] -> SetName(Form("TvsZ_%s_%s_%d",
 				 material.c_str(),eW[i].c_str(),int(energy[j-nThickness])));
 
       g[i][1][j] = new TGraph(nThickness,effThick,rVal);
       //g[i][1][j]->GetYaxis()->SetRangeUser(-1.0e-3,10);
-      g[i][1][j] -> SetTitle(Form("%s %s Reflc for PID=%d E=%d; thickness [cm]; hits [#]",
+      g[i][1][j] -> SetTitle(Form("%s %s Reflc for PID=%d E=%d; thickness [mm]; hits [#]",
 				  material.c_str(),eW[i].c_str(),pdgID,int(energy[j-nThickness])));
       g[i][1][j] -> SetName(Form("RvsZ_%s_%s_%d",
 				 material.c_str(),eW[i].c_str(),int(energy[j-nThickness])));
@@ -124,7 +125,7 @@ void analyzeOneMaterial(string material, int pdgID, TGraph *g[2][2][nThickness+n
 
 void extractOneFile(int e, double thick, string mat, int pdgID,
 		    double trans[2], double ref[2]){
-  string fnm = Form("./Neutron%04dMeV-%03.1fcm%s/E_TranRefNeutron%04dMeV-%03.1fcm%s-1e5.root",
+  string fnm = Form("./Gamma%04dkeV-%03.1fcm%s/E_TranRefGamma%04dkeV-%03.1fcm%s-1e5.root",
 		    e, thick, mat.c_str(), e, thick, mat.c_str());
   TFile *fin = TFile::Open(fnm.c_str(),"READ");
   TTree *t=(TTree*)fin->Get("t");
@@ -139,7 +140,7 @@ void extractOneFile(int e, double thick, string mat, int pdgID,
   c1->cd(1);
   t->Project("hT","prePosY:prePosX",Form("pType==%d && detID == 1 && %s",
 					 pdgID,eCut.c_str()));
-  hT->SetTitle(Form("%s Trans for %d %s| Ebeam=%d | thick = %f;x[cm];y[cm]",
+  hT->SetTitle(Form("%s Trans for %d %s| Ebeam=%d | thick = %f;x[mm];y[mm]",
 		    mat.c_str(),pdgID,eCut.c_str(),e,thick));
   trans[0] = hT->Integral()/nEv;
   hT->DrawCopy("colz");
@@ -148,7 +149,7 @@ void extractOneFile(int e, double thick, string mat, int pdgID,
   t->Project("hR","prePosY:prePosX",Form("pType==%d && detID == 0 && %s",
 					 pdgID,eCut.c_str()));
   ref[0] = hR->Integral()/nEv;
-  hR->SetTitle(Form("%s Refl for %d %s| Ebeam=%d | thick = %f;x[cm];y[cm]",
+  hR->SetTitle(Form("%s Refl for %d %s| Ebeam=%d | thick = %f;x[mm];y[mm]",
 		    mat.c_str(),pdgID,eCut.c_str(),e,thick));
   hR->DrawCopy("colz");
   gPad->SetLogz(1);
@@ -157,7 +158,7 @@ void extractOneFile(int e, double thick, string mat, int pdgID,
   c1->cd(1);
   t->Project("hT","prePosY:prePosX",Form("preKE*(pType==%d && detID == 1 && %s)",
 					 pdgID,eCut.c_str()));
-  hT->SetTitle(Form("%s Eweight Trans for %d %s| E=%d | thick = %f;x[cm];y[cm]",
+  hT->SetTitle(Form("%s Eweight Trans for %d %s| E=%d | thick = %f;x[mm];y[mm]",
 		    mat.c_str(),pdgID,eCut.c_str(),e,thick));
   //trans[1] = hT->Integral()/(nEv);
   trans[1] = hT->Integral()/(nEv*e);
@@ -168,7 +169,7 @@ void extractOneFile(int e, double thick, string mat, int pdgID,
 					 pdgID,eCut.c_str()));
   //ref[1] = hR->Integral()/(nEv);
   ref[1] = hR->Integral()/(nEv*e);
-  hR->SetTitle(Form("%s Eweight Refl for %d %s| E=%d | thick = %f;x[cm];y[cm]",
+  hR->SetTitle(Form("%s Eweight Refl for %d %s| E=%d | thick = %f;x[mm];y[mm]",
 		    mat.c_str(),pdgID,eCut.c_str(),e,thick));
   hR->DrawCopy("colz");
   gPad->SetLogz(1);
--- scripts/drawTransRef.C
+++ scripts/drawTransRef.C
@@ -1,9 +1,9 @@
 void drawTransRef(){
   //TFile *fin=TFile::Open("o_TransRefl.root","READ");
-  TFile *fin=TFile::Open("Al_TransRefl_PDGid2112_Ecut0.root","READ");
-  const int nThickness = 7;
-  const int nEnergy = 3;
-  string materials[1] = {"G4_Al"};
+  TFile *fin=TFile::Open("Lead_TransRefl_PDGid22_Ecut0.root","READ");
+  const int nThickness = 8;
+  const int nEnergy = 5;
+  string materials[1] = {"G4_Pb"};
 		/*	 "G4_W",
 			 "G4_Pb",
 			 "G4_Fe",
@@ -12,8 +12,8 @@ void drawTransRef(){
   };*/
   //double thick[nThickness]={1,2,5,10,15,20};
   //double thick[nThickness]={5,10,20,30,40,50,60};
-  double thick[nThickness]={5,10,20,30,40,50,60};
-  double energy[nEnergy] = {10,50,100};
+  double thick[nThickness]={1,2,5,10,15,20,25,50};
+  double energy[nEnergy] = {100,150,200,250,500};
   /*double thick[4]={5,7.5,10,12.5};
   double energy[7] = {500, 1000, 1500, 2000, 3000, 5000, 8000};*/
   int colorMat[1]={1};
@@ -28,13 +28,13 @@ void drawTransRef(){
     string gNm="",title="";
     for(int j=0;j<1;j++)
       for(int k=0;k<2;k++){
-	if(i<4){
+	if(i<nThickness){
 	  gNm = Form("TvsE_%s_%s_%d" ,materials[j].c_str(),typeHit[k].c_str(),int(thick[i]));
-	  title = Form("%s for 1e5 ev| %d thick;energy[MeV]",typeHit[k].c_str(),int(thick[i]));
+	  title = Form("%s for 1e5 evt| %d thick;energy[keV]",typeHit[k].c_str(),int(thick[i]));
 	}else{
-	  gNm = Form("TvsZ_%s_%s_%d" ,materials[j].c_str(),typeHit[k].c_str(),int(energy[i-4]));
-	  title = Form("%s for 1e5 ev| E=%d MeV; Thickness [cm]; fraction",
-		       typeHit[k].c_str(),int(energy[i-4]));
+	  gNm = Form("TvsZ_%s_%s_%d" ,materials[j].c_str(),typeHit[k].c_str(),int(energy[i-nThickness]));
+	  title = Form("%s for 1e5 evt| E=%d keV; Thickness [mm]; fraction",
+		       typeHit[k].c_str(),int(energy[i-nThickness]));
 	}
 	cout<<"reading "<<gNm<<"\t"<<title<<endl;
 	gr = (TGraph*)fin->Get(gNm.c_str());
@@ -46,20 +46,20 @@ void drawTransRef(){
 
 	if(j==0){
 	  gr->SetTitle(title.c_str());
-	  if(i>=4)
-	    //gr->SetMinimum(0.001);
+	  if(i>=nThickness)
 	  gr->Draw("APL");
+	//gr->SetMinimum(1.0e-10);
 	gr->GetYaxis()->SetTitleOffset(1.5);
 	//gr->GetYaxis()->SetNDdivisions(404);
 	}else
 	  gr->Draw("P");
       }
     can[i]->cd(1);
-    //gPad->SetLogy(1);
+    gPad->SetLogy(1);
     gPad->SetGridx(1);
     gPad->SetGridy(1);
     can[i]->cd(2);
-    //gPad->SetLogy(1);
+    gPad->SetLogy(1);
     gPad->SetGridx(1);
     gPad->SetGridy(1);
   }
